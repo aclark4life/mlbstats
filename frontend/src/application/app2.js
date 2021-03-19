2dc83370f58de956bcf6e4c2dea72d14a7406f6e
Alex Clark <aclark@aclark.net>
2021-03-19 17:54:06 -0400

    Add react

--- frontend/src/application/app2.js
+++ frontend/src/application/app2.js
@@ -1,7 +1,40 @@
-import "bootstrap/dist/js/bootstrap.bundle";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "../components/sidebar";
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
 
-window.document.addEventListener("DOMContentLoaded", function () {
-  window.console.log("dom ready 2");
-});
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
